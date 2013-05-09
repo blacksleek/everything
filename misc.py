fdfb5afbd9fedde9a11e10ccc08536d582608906
blacksleek <tan.phengruey.ryan@dhs.sg>
2013-05-09 15:29:54 +0800

    Update misc.py

--- misc.py
+++ misc.py
@@ -5,3 +5,5 @@ text = "akfjldas;jfla"
 print(text.ljust(20)) #20 characters total, left justified
 
 print("1, 2, 3, 4, 5".split(", ")) #returns list
+
+print("this is a cow.".strip("aeiou")) #removes all mentioned characters
