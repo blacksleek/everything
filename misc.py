2715c20d99b1de63735ba1bc3c4bff5fdb1b1203
blacksleek <tan.phengruey.ryan@dhs.sg>
2013-05-09 15:28:56 +0800

    Create misc.py

---
+++ misc.py
@@ -0,0 +1,7 @@
+import string
+print(string.ascii_lowercase) #string of letters, uppercase can be used
+
+text = "akfjldas;jfla"
+print(text.ljust(20)) #20 characters total, left justified
+
+print("1, 2, 3, 4, 5".split(", ")) #returns list
