c5701c0afa3453a275820abcca0287fe93dbdde1
blacksleek <tan.phengruey.ryan@dhs.sg>
2013-07-24 11:12:00 +0800

    Update misc.py

--- misc.py
+++ misc.py
@@ -7,3 +7,5 @@ print(text.ljust(20)) #20 characters total, left justified
 print("1, 2, 3, 4, 5".split(", ")) #returns list
 
 print("this is a cow.".strip("aeiou")) #removes all mentioned characters
+
+repeat-until loop = check for criteria last (ie run at least once)
